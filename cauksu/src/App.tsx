7f4667c39b2bf29bfe673ef9ce3851b3ac9a9cac
SomeonesDads <nael.i.manurung@gmail.com>
2026-04-14 20:43:43 +0700

    feat: init FE

---
+++ cauksu/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-gray-700 h-screen'>
+      <header className='bg-gray-800 h-20 flex items-center'>
+        <h1 className='text-white pl-8'>CAUKSU</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
